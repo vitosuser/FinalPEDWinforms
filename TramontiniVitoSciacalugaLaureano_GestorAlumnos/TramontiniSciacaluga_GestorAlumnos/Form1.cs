4cc69d7760c3622ff708a61d2ccbc18b74adde1c
vito <vitoromanloro@gmail.com>
2025-12-03 20:43:52 -0300

    creacion proyecto

---
+++ TramontiniVitoSciacalugaLaureano_GestorAlumnos/TramontiniSciacaluga_GestorAlumnos/Form1.cs
@@ -0,0 +1,10 @@
+namespace TramontiniSciacaluga_GestorAlumnos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
