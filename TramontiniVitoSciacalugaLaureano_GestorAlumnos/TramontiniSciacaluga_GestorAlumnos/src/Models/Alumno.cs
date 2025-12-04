c842635320e4b15024c3bf4e706b61ddfe2703cc
vito <vitoromanloro@gmail.com>
2025-12-03 21:11:11 -0300

    cargue clases

---
+++ TramontiniVitoSciacalugaLaureano_GestorAlumnos/TramontiniSciacaluga_GestorAlumnos/src/Models/Alumno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramontiniSciacaluga_GestorAlumnos.src.Models
+{
+    internal class Alumno
+    {
+    }
+}
